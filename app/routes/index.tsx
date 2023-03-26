cedf1e65fd9cf5e998ca3926fcfc28abe868f32a
maciej <zurawinaiserow@gmail.com>
2023-03-26 21:25:48 +0200

    add blog posts section

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -135,6 +135,14 @@ export default function Index() {
             ))}
           </div>
         </div>
+        <div className="mx-auto mt-16 max-w-7xl text-center">
+          <Link
+            to="/posts"
+            className="text-xl text-blue-600 underline"
+          >
+            Blog Posts
+          </Link>
+        </div>
       </div>
     </main>
   );
