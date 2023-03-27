c0ab5c2e3d99b52452278087844a4329dfb687a7
maciej <zurawinaiserow@gmail.com>
2023-03-27 21:43:37 +0200

    add edit blog post functionality

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -13,3 +13,10 @@ export async function getPost(slug: string) {
 export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
 	return prisma.post.create({ data: post });
 }
+
+export async function updatePost(post: Pick<Post, "slug" | "title" | "markdown">) {
+	return prisma.post.update({
+		where: { slug: post.slug },
+		data: post
+	});
+}
---
+++ app/routes/posts/admin/$slug.tsx
@@ -0,0 +1,147 @@
+import type { LoaderArgs, ActionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, useLoaderData, useActionData } from "@remix-run/react";
+import { marked } from "marked";
+import invariant from "tiny-invariant";
+import * as React from "react";
+
+import { getPost, updatePost } from "~/models/post.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  const html = marked(post.markdown);
+  return json({ post, html });
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some(
+    (errorMessage) => errorMessage
+  );
+  if (hasErrors) {
+    return json(errors);
+  }
+
+  invariant(
+    typeof title === "string",
+    "title must be a string"
+  );
+  invariant(
+    typeof slug === "string",
+    "slug must be a string"
+  );
+  invariant(
+    typeof markdown === "string",
+    "markdown must be a string"
+  );
+
+  await updatePost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
+
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
+export default function PostSlug() {
+  const { post } = useLoaderData<typeof loader>();
+  const errors = useActionData<typeof action>();
+
+  const [formData, setFormData] = React.useState({
+    slug: post.slug,
+    title: post.title,
+    markdown: post.markdown,
+  });
+
+  React.useEffect(() => {
+    setFormData(post)
+  }, [post])
+
+  function handleChange(
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const { name, value } = event.currentTarget;
+    setFormData({ ...formData, [name]: value });
+  }
+  
+  return (
+    <main className="mx-auto max-w-4xl">
+      <Form method="post">
+        <p>
+          <label>
+            Post Title:{" "}
+            {errors?.title ? (
+              <em className="text-red-600">{errors.title}</em>
+            ) : null}
+            <input
+              type="text"
+              name="title"
+              className={inputClassName}
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Post Slug:{" "}
+            {errors?.slug ? (
+              <em className="text-red-600">{errors.slug}</em>
+            ) : null}
+            <input
+              type="text"
+              name="slug"
+              className={inputClassName}
+              value={formData.slug}
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="markdown">
+            Markdown:{" "}
+            {errors?.markdown ? (
+              <em className="text-red-600">
+                {errors.markdown}
+              </em>
+            ) : null}
+          </label>
+
+          <br />
+          <textarea
+            id="markdown"
+            rows={20}
+            name="markdown"
+            className={`${inputClassName} font-mono`}
+            value={formData.markdown}
+            onChange={handleChange}
+          />
+        </p>
+        <p className="text-right">
+          <button
+            type="submit"
+            className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          >
+            Update Post
+          </button>
+        </p>
+      </Form>
+    </main>
+
+  );
+}
